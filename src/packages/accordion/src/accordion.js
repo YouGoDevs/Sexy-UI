30d8fea90225edc82a6df4f026446f19041f21e5
geller99 <gellera800@gmail.com>
2022-01-13 01:21:04 +0100

    Added closeOnOutsideClick Method

--- src/packages/accordion/src/accordion.js
+++ src/packages/accordion/src/accordion.js
@@ -1,34 +1,48 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import React from "react";
 import { AccordionPanel } from "./accordionpanel";
-import { AccordionTitle } from "./accordiontitle";
 
-export const Accordion = ({
-  children,
-  multiple,
-  active,
-  onToggle,
-}) => {
- /* 
+export const Accordion = ({ children }) => {
+  /* 
   local visibility toggle to manage visibility in individual components
  */
-  const [visibility, setVisibility] = useState(false);
+
+  const visibilityRef = useRef();
+
+  const [visibility, setVisibility] = useState(true);
 
   const toggleVisible = () => {
     setVisibility((prev) => !prev);
   };
 
+  /* 
+  
+  This method closes tabs if you click outside of the div in visibilityRef.current
+
+ */
+  const closeOnOutsideClick = (e) => {
+    if (!visibilityRef.current === e.target) {
+      setVisibility(false);
+    }
+  };
+
+  console.log(visibilityRef);
   // if no accordions exist return null
   if (!children) return null;
 
   return (
     <div
       style={{ border: "1px solid red" }}
-      onClick={toggleVisible}
+      // attempting to run both methods side by side with an inline function
+      onClick={(e) => {
+        closeOnOutsideClick(e);
+        toggleVisible();
+      }}
+      ref={visibilityRef}
     >
       <> {children[0]} </>
 
-      {!visibility  ? null : (
+      {visibility === false ? null : (
         <AccordionPanel> {children[1]} </AccordionPanel>
       )}
     </div>
